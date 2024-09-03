2d99001d74ce41261cc9e307e942e181f96c41a7
Luciano Seguel <96950349+lucianoseguel@users.noreply.github.com>
2024-09-03 11:14:08 -0300

    1.0

---
+++ scripts/index-fx.js
@@ -0,0 +1,44 @@
+window.sr = ScrollReveal();
+
+// TITULO
+    sr.reveal('#tituloprincipal',{
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-50px'
+    });
+
+
+
+
+//TEXTO 1
+    sr.reveal('#subtituloprincipal',{
+            duration: 1000,
+            
+            });
+
+
+
+
+//H2 
+
+sr.reveal('h2',{
+    duration: 700,
+    origin: 'bottom',
+    distance: '50px'
+    });
+
+sr.reveal('#objetivo',{
+        duration: 700,
+        origin: 'bottom',
+        distance: '150px'
+        });
+    
+sr.reveal('.col',{
+            duration: 500,
+            origin: 'bottom',
+            distance: '200px'
+            });
+
+
+        
+
